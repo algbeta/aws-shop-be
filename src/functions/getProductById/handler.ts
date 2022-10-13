1ab60646818bce6b94c73ac75ead94c34b67599e
Natallia Gorbunova <gorbunovano@gmail.com>
2022-10-13 17:33:15 +0200

    feat(task-4): initial code

--- src/functions/getProductById/handler.ts
+++ src/functions/getProductById/handler.ts
@@ -2,22 +2,33 @@ import type { ValidatedEventAPIGatewayProxyEvent } from "../../libs/api-gateway"
 import { formatJSONResponse } from "../../libs/api-gateway";
 import { middyfy } from "../../libs/lambda";
 import createError from "http-errors";
-import { productService } from "../../services";
+import { productService, stockService } from "../../services";
 import schema from "./schema";
+import { Stock } from "src/services/stock-service";
+import { Product } from "src/services/product-service";
 
 export const getProductById: ValidatedEventAPIGatewayProxyEvent<
   typeof schema
 > = async (event) => {
-  const {
-    pathParameters: { id },
-  } = event;
-  const product = productService.getProduct(id);
-  if (!product) {
-    throw new createError.NotFound("Product not found");
+  try {
+    const {
+      pathParameters: { id },
+    } = event;
+    const product: Product = await productService.getProduct(id);
+    const stocks: Stock[] = await stockService.getStockByProductId(id);
+    if (!product) {
+      return formatJSONResponse({
+        statusCode: 404,
+        message: "Product not found",
+      });
+    }
+    return formatJSONResponse({
+      // @ts-ignore
+      items: [{ ...product, count: stocks?.length > 0 ? stocks[0].stock : 0 }],
+    });
+  } catch (e) {
+    throw new createError.InternalServerError();
   }
-  return formatJSONResponse({
-    items: [product],
-  });
 };
 
 export const main = middyfy(getProductById);
